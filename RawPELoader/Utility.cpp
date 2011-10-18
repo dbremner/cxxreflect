3d8fe8396de3362850d9e93a9516b072d8b411b6
james <james@jamesmcnellis.com>
2011-10-17 20:43:58 -0700

    Removing v.1 and v.2 of the 'RawPELoader' physical layer; that code has now been integrated into CxxReflect.

--- RawPELoader/Utility.cpp
+++
@@ -1,3 +0,0 @@
-//                 Copyright (c) 2011 James P. McNellis <james@jamesmcnellis.com>                 //
-//                   Distributed under the Boost Software License, Version 1.0.                   //
-//     (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)    //
