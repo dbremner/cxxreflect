2907f2002ca878a27b337c1122858bb19bcf6d66
james <james@localhost>
2012-03-24 00:17:47 -0700

    Adding a TestAssemblies test project.

---
+++ Tests/TestAssemblies/Stub.cpp
@@ -0,0 +1,7 @@
+//                 Copyright (c) 2012 James P. McNellis <james@jamesmcnellis.com>                 //
+//                   Distributed under the Boost Software License, Version 1.0.                   //
+//     (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)    //
+
+// This file exists solely so that there is something for MSBuild to build.
+
+int main() { }
