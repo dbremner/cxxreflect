7b20ebc8a0319ac26d6b2dd3dd07a40079c1cc08
james <james@jamesmcnellis.com>
2012-04-25 23:51:32 -0700

    Rearranging the WRTestApp demos for clarity and consistency.

--- Tests/WRTestApp/BlankPage.xaml.cpp
+++ Tests/WRTestApp/BlankPage.xaml.cpp
@@ -27,46 +27,46 @@ namespace WRTestApp {
 
         cxr::WhenInitializedCall([&]
         {
-            auto const dispatcherObjectTypes(cxr::GetImplementersOf<Windows::UI::Xaml::IDependencyObject>());
-
-            auto const types(cxr::GetImplementersOf<WRLibrary::IProvideANumber>());
-            
-            std::for_each(begin(types), end(types), [&](cxr::Type const& type)
             {
-                if (!cxr::IsDefaultConstructible(type))
-                    return;
-
-                auto const instance(cxr::CreateInstance<WRLibrary::IProvideANumber>(type));
-        
-                PrintTypeAndNumber(instance);
-            });
-            
-            Platform::String::typeid->FullName->Data();
-
-            WRLibrary::ProviderOfTheAnswer^ provider = ref new WRLibrary::ProviderOfTheAnswer();
-            cxr::Type const wrapperType = cxr::GetType(L"WRLibrary.ProviderOfAWrappedNumber");
-            auto const wrapperInstance = cxr::CreateInstance<WRLibrary::IProvideANumber>(wrapperType, provider);
-            PrintTypeAndNumber(wrapperInstance);
-
-            cxr::Type const userType(cxr::GetType(L"WRLibrary.UserProvidedNumber"));
-            for (int i(0); i < 10; ++i)
-            {
-                auto const userInstance(cxr::CreateInstance<WRLibrary::IProvideANumber>(userType, i * 5));
-
-                std::wstringstream formatter;
-                formatter << userType.GetFullName() << L":  " << userInstance->GetNumber() << L'\n';
-
-                OutputDebugString(formatter.str().c_str());
+                // typedef Windows::UI::Xaml::IDependencyObject IDependencyObject;
+                // auto const dependencyObjectTypes = cxr::GetImplementersOf<IDependencyObject>();
             }
 
-            cxr::Type const userMultipliedType(cxr::GetType(L"WRLibrary.UserProvidedMultipliedNumber"));
+            {
+                auto const types(cxr::GetImplementersOf<WRLibrary::IProvideANumber>());
+                std::for_each(begin(types), end(types), [](cxr::Type const& type)
+                {
+                    if (!cxr::IsDefaultConstructible(type))
+                        return;
 
-            auto const userMultipliedInstance(cxr::CreateInstance<WRLibrary::IProvideANumber>(userMultipliedType, 2, 4));
+                    auto const instance(cxr::CreateInstance<WRLibrary::IProvideANumber>(type));
+        
+                    PrintTypeAndNumber(instance);
+                });
+            }
 
-            std::wstringstream formatter;
-            formatter << userMultipliedType.GetFullName() << L":  " << userMultipliedInstance->GetNumber() << L'\n';
+            {
+                cxr::Type const type(cxr::GetType(L"WRLibrary.UserProvidedNumber"));
+                for (int i(0); i < 5; ++i)
+                {
+                    auto const instance(cxr::CreateInstance<WRLibrary::IProvideANumber>(type, i));
 
-            OutputDebugString(formatter.str().c_str());
+                    PrintTypeAndNumber(instance);
+                }
+            }
+
+            {
+                WRLibrary::ProviderOfTheAnswer^ provider = ref new WRLibrary::ProviderOfTheAnswer();
+                cxr::Type const wrapperType = cxr::GetType(L"WRLibrary.ProviderOfAWrappedNumber");
+                auto const wrapperInstance = cxr::CreateInstance<WRLibrary::IProvideANumber>(wrapperType, provider);
+                PrintTypeAndNumber(wrapperInstance);
+            }
+
+            {
+                cxr::Type const userMultipliedType(cxr::GetType(L"WRLibrary.UserProvidedMultipliedNumber"));
+                auto const userMultipliedInstance(cxr::CreateInstance<WRLibrary::IProvideANumber>(userMultipliedType, 2, 4));
+                PrintTypeAndNumber(userMultipliedInstance);
+            }
 
         });
     }
