a12dc95345bcd66b746316894ad3c8af6eea8793
JamesMcNellis <JamesMcNellis@localhost>
2012-06-21 00:29:03 -0700

    Removing old implementation to be replaced with the new one from the other repository

--- Tests/TestAssemblies/Stub.cpp
+++
@@ -1,8 +0,0 @@
-
-//                            Copyright James P. McNellis 2011 - 2012.                            //
-//                   Distributed under the Boost Software License, Version 1.0.                   //
-//     (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)    //
-
-// This file exists solely so that there is something for MSBuild to build.
-
-int main() { }
