4dc6e8d19a21b90082b3bb6c57422dc8ced6c86c
JamesMcNellis <JamesMcNellis@localhost>
2013-01-01 00:58:08 -0800

    o hai 2013

--- utility/ilasm_and_embed.bat
+++ utility/ilasm_and_embed.bat
@@ -1,5 +1,5 @@
 
-REM "//               Copyright James P. McNellis (james@jamesmcnellis.com) 2011 - 2012.               //"
+REM "//                            Copyright James P. McNellis 2011 - 2013.                            //"
 REM "//                   Distributed under the Boost Software License, Version 1.0.                   //"
 REM "//     (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)    //"
 
